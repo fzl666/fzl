db172a2c0b18a9c49103448c018f173256d567e1
fzl666 <60146911+fzl666@users.noreply.github.com>
2020-07-03 12:44:26 -0400

    Update sketch.js

--- subculturenames/sketch.js
+++ subculturenames/sketch.js
@@ -23,20 +23,26 @@ function setup() {
   fzl = createButton('@fzlmagazine')
   fzl.position(360,460)
   fzl.mousePressed(socialMedia)
- // saveThis = createButton('保存')
-  //saveThis.position(450,600)
+  //saveThis = createButton('保存')
+ // saveThis.position(450,600)
  // saveThis.mousePressed(snapShot)
   noLoop()
 }
 
 function draw() {
   
-  createCanvas(500, 500)
-  background(225);
+  createCanvas(500, 700)
+  fill(225);
+  noStroke()
+  rect(0,0,500,500)
   image(title, 50, 0, 400, 100)
   textSize(10)
   fill(100)
-  text('Programmed by LuwenxiSong, scripted by LuwenxiSong & BenitaGuo from @fzlmagazine', 40, 495)
+  text('Programmed by LuwenxiSong, scripted by LuwenxiSong & BenitaGuo from @fzlmagazine', 40, 498)
+  fill('blue')
+  textSize(14)
+  text('仅供个人娱乐，禁止盗用',190,600)
+  
 
   if (img) {
     image(img, 170, 120, 280, 280)
